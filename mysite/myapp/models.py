6aa4e27b486b30e980bcd8dc3ef889dd46aca095
sajjad <sajjadzolghadr777@gmail.com>
2026-08-25 15:27:27 +0330

    feat:Add seller field to Product model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Product(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.FloatField()
