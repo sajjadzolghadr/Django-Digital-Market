ff9570f7e2b072e635f5cbe5cbec93413f1a3b4a
sajjad <sajjadzolghadr777@gmail.com>
2026-08-31 13:47:20 +0330

    feat:Add Customer model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -32,3 +32,12 @@ class Purchase(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
