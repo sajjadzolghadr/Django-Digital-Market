e45f51107e9b5f0c06cf862e2e55e49ec26fd561
sajjad <sajjadzolghadr777@gmail.com>
2026-09-08 13:18:30 +0330

    feat:Add wishlist model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -40,3 +40,12 @@ class Purchase(models.Model):
 
 
 
+class Wishlist(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('customer', 'product')
+
+
