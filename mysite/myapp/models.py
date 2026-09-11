fd83a53646f16c222317221f6edb262a16491c47
sajjad <sajjadzolghadr777@gmail.com>
2026-09-11 12:04:17 +0330

    feat:Add Review model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -49,3 +49,15 @@ class Wishlist(models.Model):
         unique_together = ('customer', 'product')
 
 
+class Review(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('customer', 'product')
+
+    def __str__(self):
+        return f"{self.product.name} - {self.rating}/5"
