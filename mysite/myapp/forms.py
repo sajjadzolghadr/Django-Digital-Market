7cfa65ce29b113d942077a02296337b01ffe2c53
sajjad <sajjadzolghadr777@gmail.com>
2026-09-14 12:57:25 +0330

    feat:Add profile form

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -108,3 +108,17 @@ class ReviewForm(forms.ModelForm):
                 'placeholder': 'Write your review...'
             }),
         }
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control'
+            }),
+        }
