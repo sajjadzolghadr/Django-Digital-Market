ad934ed06217d1d0469e8500d0ff8e34f8f2b522
sajjad <sajjadzolghadr777@gmail.com>
2026-09-15 10:52:40 +0330

    feat:Add password change success message

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -371,6 +371,7 @@ def change_password(request):
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)
+            messages.success(request, 'Password changed successfully.')
             return redirect('profile')
     else:
         form = PasswordChangeForm(request.user)
