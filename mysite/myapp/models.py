be5aee5400e572ffafcea1626296023ab259159b
sajjad <sajjadzolghadr777@gmail.com>
2026-09-16 11:24:24 +0330

    feat:Add Notification model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -61,3 +61,13 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.product.name} - {self.rating}/5"
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=255)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
