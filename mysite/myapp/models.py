875cf30822eaa521df2fc38060f5a84b4c8c335f
sajjad <sajjadzolghadr777@gmail.com>
2026-09-18 18:01:21 +0330

    feat:Add quantity to cart items

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -36,6 +36,7 @@ class OrderDetail(models.Model):
 class Purchase(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
